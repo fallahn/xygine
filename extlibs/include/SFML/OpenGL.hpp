7e3ace19e76e3c8a64db0e02bee7b38a8945f891
fallahn <matty_styles@hotmail.com>
2018-04-10 12:47:03 +0100

    update extlibs for visual studio project

--- extlibs/include/SFML/OpenGL.hpp
+++ extlibs/include/SFML/OpenGL.hpp
@@ -1,7 +1,7 @@
 ////////////////////////////////////////////////////////////
 //
 // SFML - Simple and Fast Multimedia Library
-// Copyright (C) 2007-2017 Laurent Gomila (laurent@sfml-dev.org)
+// Copyright (C) 2007-2018 Laurent Gomila (laurent@sfml-dev.org)
 //
 // This software is provided 'as-is', without any express or implied warranty.
 // In no event will the authors be held liable for any damages arising from the use of this software.
@@ -69,6 +69,7 @@
     #include <GLES/glext.h>
     
     // We're not using OpenGL ES 2+ yet, but we can use the sRGB extension
+    #include <GLES2/gl2platform.h>
     #include <GLES2/gl2ext.h>
 
 #endif
