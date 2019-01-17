e69bf7c85346d6657e77ffc03cadcb0e0cfebecf
fallahn <matty_styles@hotmail.com>
2019-01-17 15:27:49 +0000

    add project for simple particle editor

---
+++ SimpleParticleEditor/src/MyFirstState.cpp
@@ -0,0 +1,64 @@
+/*********************************************************************
+(c) Jonny Paton 2018
+(c) Matt Marchant 2019
+
+xygineXT - Zlib license.
+
+This software is provided 'as-is', without any express or
+implied warranty. In no event will the authors be held
+liable for any damages arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute
+it freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented;
+you must not claim that you wrote the original software.
+If you use this software in a product, an acknowledgment
+in the product documentation would be appreciated but
+is not required.
+
+2. Altered source versions must be plainly marked as such,
+and must not be misrepresented as being the original software.
+
+3. This notice may not be removed or altered from any
+source distribution.
+*********************************************************************/
+
+#include "MyFirstState.hpp"
+#include "States.hpp"
+
+MyFirstState::MyFirstState(xy::StateStack& ss, xy::State::Context ctx) :
+xy::State(ss,ctx),
+m_scene(ctx.appInstance.getMessageBus())
+{
+
+}
+
+bool MyFirstState::handleEvent(const sf::Event& evt)
+{
+    m_scene.forwardEvent(evt);
+    return true;
+}
+
+void MyFirstState::handleMessage(const xy::Message& msg)
+{
+    m_scene.forwardMessage(msg);
+}
+
+bool MyFirstState::update(float dt)
+{
+    m_scene.update(dt);
+    return true;
+}
+
+void MyFirstState::draw()
+{
+    auto rw = getContext().appInstance.getRenderWindow();
+    rw->draw(m_scene);
+}
+
+xy::StateID MyFirstState::stateID() const
+{
+    return States::MyFirstState;
+}
